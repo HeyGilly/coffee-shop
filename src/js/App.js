7e58aa7df28684169c1c23d147edb95ef2e7fead
heygilly <nick.gilmore20@gmail.com>
2023-10-30 13:29:37 -0500

    Jumbotron added

--- src/js/App.js
+++ src/js/App.js
@@ -1,11 +1,13 @@
 import '../css/resetCSS.css';
 import Navbar from './Navbar';
+import Jumbotron from './jumbotron';
 import '../css/App.css';
 
 function App() {
   return (
      <>
       <Navbar />
+     <Jumbotron />
      </>
 
   );
