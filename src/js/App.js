0a27cb6a7ef489e28ef9d3b47724f0909247b365
heygilly <nick.gilmore20@gmail.com>
2023-10-30 12:54:20 -0500

    imported a navbar

---
+++ src/js/App.js
@@ -0,0 +1,14 @@
+import '../css/resetCSS.css';
+import Navbar from './Navbar';
+import '../css/App.css';
+
+function App() {
+  return (
+     <>
+      <Navbar />
+     </>
+
+  );
+}
+
+export default App;
