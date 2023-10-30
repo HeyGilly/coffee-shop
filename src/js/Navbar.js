5f43aa32549aafc2249389523c8e17af6df42ff8
heygilly <nick.gilmore20@gmail.com>
2023-10-30 12:56:29 -0500

    Navbar added

---
+++ src/js/Navbar.js
@@ -0,0 +1,10 @@
+function Navbar(){
+    return(
+        <nav className="navbar bg-body-tertiary shadow">
+            <div className="container py-3">
+                <p className={""}>Casual Coffee</p>
+            </div>
+        </nav>
+    )
+}
+export default Navbar;
